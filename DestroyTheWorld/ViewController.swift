1f350775811cc98a89ac9b3eacc9761c8a188496
Onintza garmendia <onintza.garmendia@gmail.com>
2017-01-31 00:35:48 +0100

    Initial Commit

---
+++ DestroyTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld
+//
+//  Created by Onintza Garmendia on 31/01/2017.
+//  Copyright © 2017 hamahiru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
